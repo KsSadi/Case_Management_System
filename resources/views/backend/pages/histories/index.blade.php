99816d6112b24357f464dd7ee15c6f5301682626
Sadi <mdsadi.ossp@gmail.com>
2026-05-14 10:24:22 +0600

    Init

--- resources/views/backend/pages/histories/index.blade.php
+++ resources/views/backend/pages/histories/index.blade.php
@@ -71,43 +71,23 @@
 
                     </td>
                     <td class="hidden md:table-cell">
-                        <span class="font-medium">@if($history->cases)
-                                {{ $history->cases->projects->name}}
-                            @else
-                                Not Found
-                            @endif</span>
+                        <span class="font-medium">{{ $history->cases?->projects?->name ?? 'Not Found' }}</span>
 
                     </td>
                     <td class="hidden lg:table-cell">
-                        <span class="font-medium">@if($history->cases)
-                                {{ $history->cases->divisions->name}}
-                            @else
-                                Not Found
-                            @endif</span>
+                        <span class="font-medium">{{ $history->cases?->divisions?->name ?? 'Not Found' }}</span>
 
                     </td>
                     <td class="hidden lg:table-cell">
-                        <span class="font-medium">@if($history->cases)
-                                {{ $history->cases->types->name }}
-                            @else
-                                Not Found
-                            @endif</span>
+                        <span class="font-medium">{{ $history->cases?->types?->name ?? 'Not Found' }}</span>
 
                     </td>
                     <td class="hidden xl:table-cell">
-                        <span class="font-medium">@if($history->cases)
-                                {{ $history->cases->courts->name }}
-                            @else
-                                Not Found
-                            @endif</span>
+                        <span class="font-medium">{{ $history->cases?->courts?->name ?? 'Not Found' }}</span>
 
                     </td>
                     <td class="hidden md:table-cell">
-                        <span class="font-medium"> @if($history->cases)
-                                {{ $history->cases->advocates->name }}
-                            @else
-                                Not Found
-                            @endif </span>
+                        <span class="font-medium">{{ $history->cases?->advocates?->name ?? 'Not Found' }}</span>
 
                     </td>
                     <td>
