09820f9c56e0b637c470a5a970eaf9e039b52c08
Sadi <mdsadi.ossp@gmail.com>
2026-05-30 20:50:37 +0600

    init

--- database/migrations/2026_05_30_000001_add_company_id_to_case_items_table.php
+++ database/migrations/2026_05_30_000001_add_company_id_to_case_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCompanyIdToCaseItemsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class AddCompanyIdToCaseItemsTable extends Migration
             $table->dropColumn('company_id');
         });
     }
-}
+};
